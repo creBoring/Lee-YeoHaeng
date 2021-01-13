55019255b09ed2d08cfbf5bf6c1dd4d6e3b34220
creBoring <cre.boring@gmail.com>
2021-01-13 12:59:05 +0900

    Add Scheduler for changing Login Page Map

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -8,6 +8,8 @@
 
 import { Loader } from "@googlemaps/js-api-loader"
 
+
+// Google map init
 let map;
 const loader = new Loader({
   apiKey: "AIzaSyBxDQu2tvQ54YR0Y-LfIodOH_2k6IqUhyE",
@@ -15,9 +17,30 @@ const loader = new Loader({
 });
 loader.load().then(() => {
   map = new google.maps.Map(document.getElementById("google-map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    center: { lat: 35.6809529, lng: 139.7665669 }, //도쿄역
+    zoom: 16,
+    disableDefaultUI: true,
+    mapId: '5b468ee5a742e3c1'
   });
 });
 
-console.log("test");
+
+// login Page Map 계속 위치 바꿔주기
+var cityArray = [
+  { lat: 35.697, lng: 139.771 },  // 아키하바라
+  { lat: 35.630, lng: 139.773 },  // 오다이바 해변공원
+  { lat: 35.688, lng: 139.700 },  // 신주쿠
+  { lat: 35.71439990929624, lng: 139.77816281882775 },  // 우에노
+  { lat: 35.658, lng: 139.744 }   // 도쿄타워
+]
+var n = 0;
+
+function changeLoginPageCity() {
+  map.setCenter(cityArray[n]);
+  n++;
+  if(n>cityArray.length - 1) {
+    n = 0;
+  }
+}
+
+setInterval(changeLoginPageCity, 3500);
